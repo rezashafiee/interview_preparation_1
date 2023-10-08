2f3ec14111fddb8d1e5ca01413126003b1777078
Mohammadreza Shafiee <mrshafiee@hotmail.com>
2023-10-08 18:07:02 +0200

    update unit tests

--- app/src/test/java/com/tilda/interviewpreparation1/ExampleUnitTest.kt
+++ app/src/test/java/com/tilda/interviewpreparation1/ExampleUnitTest.kt
@@ -14,4 +14,9 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun imchini_test_function() {
+        assertEquals(5, 2 + 3)
+    }
 }
